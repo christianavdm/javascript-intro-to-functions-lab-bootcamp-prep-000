6641bd9e882ea066b4071cd3b9fd88be6c819ee6
Christiana Vandermale <christianavdm@gmail.com>
2018-03-19 14:18:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper (string) {
   return string.toLowerCase()
 }
 function logShout (string) {
-  console.log(string.toUpperCase())
+  console.log(toUpperCase(string))
 }
 function sayHiToGrandma (string) {
   if (string.toLowerCase) return `I can't hear you!`
