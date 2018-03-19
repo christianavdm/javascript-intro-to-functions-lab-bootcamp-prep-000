f85cbe8bc1ddb154b90d96624d5d17c26d6c38fa
Christiana Vandermale <christianavdm@gmail.com>
2018-03-19 14:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function logShout (string) {
 function sayHiToGrandma (string) {
   if (string.toLowerCase) return `I can't hear you!`
   if (string.toUpperCase) return `YES INDEED!`
-  
+  if (I love you, Grandma.) return `I love you, too.`
 }
